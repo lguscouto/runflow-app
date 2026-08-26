9aef659658cc5b9923a62bfd4445a718e4e3dd73
Gustavo <gustavo@runflow.app>
2026-08-25 22:07:45 -0300

    test(android): align release metadata contract

--- android/app/src/test/java/com/runflow/app/AppConfigUnitTest.java
+++ android/app/src/test/java/com/runflow/app/AppConfigUnitTest.java
@@ -9,7 +9,7 @@ public class AppConfigUnitTest {
     @Test
     public void buildConfigMatchesReleaseContract() {
         assertEquals("com.runflow.app", BuildConfig.APPLICATION_ID);
-        assertEquals("0.9.6", BuildConfig.VERSION_NAME);
-        assertEquals(3, BuildConfig.VERSION_CODE);
+        assertEquals("0.9.8", BuildConfig.VERSION_NAME);
+        assertEquals(4, BuildConfig.VERSION_CODE);
     }
 }
